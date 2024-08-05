da2556b412c64e35a6847f89220c93e10e07fad5
백화랑 <kjuns123@gamil.com>
2024-08-05 22:21:24 +0900

    폴더, zip 설정 추가

--- download.js
+++ download.js
@@ -11,6 +11,19 @@ const fs = require("fs")
 const { exec } = require('node:child_process');
 const path = require("node:path")
 
+/** @type {"dir" | "zip"} */
+const type = "zip";
+const dirName = "extensions";
+
+if(type === "dir"){
+    if(fs.existsSync(path.join(__dirname, dirName))){
+        fs.rm(path.join(__dirname, dirName), {recursive: true}, (err) => {
+            if(err) console.error("dir remove error:", err)
+        })
+    }
+    fs.mkdirSync(path.join(__dirname, dirName));
+}
+
 exec('code --list-extensions --show-versions', (err, stdout, stderr) => {
     if (err) {
       console.error(err);
@@ -23,44 +36,56 @@ exec('code --list-extensions --show-versions', (err, stdout, stderr) => {
     }
 
     const job = [];
-    stdout.split("\n").slice(0, -1).forEach((ext) => {
+    const extensions = stdout.split("\n").slice(0, -1);
+    const successExtension = [];
+    [...extensions].forEach((ext) => {
+        let size = extensions.length;
+
         const [identifier, version] = ext.split("@");
         const [publisher, name] = identifier.split(".");
         const url = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${publisher}/vsextensions/${name}/${version}/vspackage`
         
         const result = fileDownload(url);
         result.then(({fileName}) => {
-            console.log("identifier", identifier)
-            console.log("fileName", fileName)
-            console.log("==========================")
+            for(let i=0; i<size; i++){
+                if(ext === extensions[i]) successExtension.push(extensions.splice(i, 1));
+            }
+            console.log("success extension", successExtension)
+            console.log("remain extension", extensions);
+            //console.log("identifier", identifier)
+            //console.log("fileName", fileName)
+            console.log("=======================================")
         })
         job.push(result);
     });
 
-    Promise.all(job)
-    .then((result) => {
-        const archive = archiver('zip', {
-            zlib: { level: 9 } // 압축 수준: 0-9 (0은 압축 없음, 9는 최대 압축)
-        });
-
-        const output = fs.createWriteStream(path.join(__dirname, 'output.zip'));
-        archive.pipe(output);
-
-        result.forEach(({fileName, data}) => {
-            archive.append(data, {name: fileName});
+    if(type === "zip"){
+        Promise.all(job)
+        .then((result) => {
+            const archive = archiver('zip', {
+                zlib: { level: 9 } // 압축 수준: 0-9 (0은 압축 없음, 9는 최대 압축)
+            });
+            
+            const output = fs.createWriteStream(path.join(__dirname, 'output.zip'));
+            archive.pipe(output);
+            
+            result.forEach(({fileName, data}) => {
+                archive.append(data, {name: fileName});
+            })
+            
+            output.on('close', () => {
+                console.log(`ZIP 파일이 ${archive.pointer()} 바이트로 생성되었습니다.`);
+              });
+              
+            archive.on('error', (err) => {
+                console.error(err);
+                throw err;
+            });
+    
+            archive.finalize();
+            console.log("압축 시작")
         })
-
-        output.on('close', () => {
-            console.log(`ZIP 파일이 ${archive.pointer()} 바이트로 생성되었습니다.`);
-          });
-          
-        archive.on('error', (err) => {
-            console.error(err);
-            throw err;
-        });
-
-        archive.finalize();
-    })
+    }
 });
 
 /**
@@ -70,11 +95,21 @@ exec('code --list-extensions --show-versions', (err, stdout, stderr) => {
  */
 const fileDownload = (url) => {
     return axios.get(url, {
-        responseType: "stream"
+        responseType: "arraybuffer"
     })
     .then((response) => {
         const {data, headers} = response;
-        const fileName = headers["content-disposition"].replace(/(.*filename=)(.*)(;.*)/, "$2")
+        const fileName = headers["content-disposition"].replace(/(.*filename=\"?)(.*)(\"?;.*)/, "$2")
+        
+        try {
+            if(type === "dir"){
+                fs.writeFile(path.join(__dirname, dirName, "/", fileName), data, (err) => {
+                    if(err) console.error("writeFile Error:", err)
+                });
+            }
+        } catch(e){
+            console.error("fileDownload Error:", e);
+        }
         
         return {fileName, data}
     })
